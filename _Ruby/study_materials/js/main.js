cfa5283bcdf36bf5df431a40aa43149717f31fc7
aesolomon4189 <asolomon@leadingre.com>
2017-04-29 16:38:09 -0500

    Study materials

---
+++ _Ruby/study_materials/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+  questions =
+  [
+    "How do we find out what folder we're working in?",
+    "How do we get to the parent of the folder we're working in?",
+    "",
+    "",
+    ""
+  ];
+});
